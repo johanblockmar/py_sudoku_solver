cf4fff30560b45b9f60105cf3a4535ea052f4160
Johan Blockmar <johan.blockmar@flexworks.com>
2021-11-09 09:06:17 +0100

    Improved prettyPrintGrid

--- py_sudoku_solver.py
+++ py_sudoku_solver.py
@@ -1,15 +1,33 @@
 
 xSize = 9
+xDiv = 3
 ySize = 9
+yDiv = 3
 
 mainGrid = [[0 for x in range(xSize)] for y in range(ySize)] 
 
-def prettyPrintGrid(grid):
+'''
+Print grid in the look of a sudoku.
+grid: matrix to print.
+divX: width of sudoku sub areas.
+divY: height of sudoku sub areas.
+'''
+def prettyPrintGrid(grid, divX, divY):
     for y in range(len(grid)):
-        print(grid[y])
+        if(y%divY==0):
+            print("")
+
+        lineStr = ""
+        for x in range(len(grid[0])):
+            if(x%divX==0):
+                lineStr+=" "
+            
+            lineStr += str(grid[x][y]) + " "
+
+        print(lineStr)
 
 def main():
-    prettyPrintGrid(mainGrid)
+    prettyPrintGrid(mainGrid, xDiv, yDiv)
     
 if __name__ == "__main__":
     main()
