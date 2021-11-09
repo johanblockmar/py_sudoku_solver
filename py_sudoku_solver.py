779640172432cb75d76f8aec6f7a485a25ba303c
Johan Blockmar <johan.blockmar@flexworks.com>
2021-11-09 08:55:19 +0100

    Added matrix to hold solution.

--- py_sudoku_solver.py
+++ py_sudoku_solver.py
@@ -1,6 +1,15 @@
 
+xSize = 9
+ySize = 9
+
+mainGrid = [[0 for x in range(xSize)] for y in range(ySize)] 
+
+def prettyPrintGrid(grid):
+    for y in range(len(grid)):
+        print(grid[y])
+
 def main():
-    print("")
+    prettyPrintGrid(mainGrid)
     
 if __name__ == "__main__":
     main()
