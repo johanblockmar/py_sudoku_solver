726d9355bff7bf31b9ebc22ddd7452850986881d
Johan Blockmar <johan.blockmar@flexworks.com>
2021-11-10 09:20:38 +0100

    Added better parsing for sudoku string.

--- py_sudoku_solver.py
+++ py_sudoku_solver.py
@@ -22,11 +22,68 @@ def prettyPrintGrid(grid, divX, divY):
             if(x%divX==0):
                 lineStr+=" "
             
-            lineStr += str(grid[x][y]) + " "
+            lineStr += str(grid[y][x]) + " "
 
         print(lineStr)
 
+def CheckSudokuLine(line, sizeX):
+    if(len(line) != 2 * sizeX - 1):
+        return False
+
+    return True
+
+
+def enterSudoku(sizeX, sizeY, sudokuString = None):
+
+    grid = [[0 for x in range(sizeX)] for y in range(sizeY)] 
+    
+    if(sudokuString == None):
+        sudokuString = ""
+
+        print("Please enter the sudoku, line by line.")
+        print("Each character should be seperated by a \",\"")
+        print("Please use \"0\" for the empty spaces.")
+        print("Write \"q\" to exit.\n")
+
+        for y in range(len(grid)):
+            done = False
+            while(not done):
+                print("Please enter line {} of the sudoku".format(y+1))
+                line = input()
+                if(line == 'q'):
+                    return None
+
+                if(CheckSudokuLine(line,sizeX)):
+                    sudokuString += line
+                    if(y < len(grid)-1):
+                        sudokuString += "\n"
+                    done = True
+                else:
+                    print("Invalid, try again")
+
+    # Parse string
+    lines = sudokuString.split("\n")
+    
+    if(len(lines) != sizeY):
+        return None
+    
+    for y in range(len(grid)):
+        l = lines[y].split(',')
+        grid[y] = l
+
+    return grid
+
+
+
 def main():
+    print("Welcome to the Sudoku Solver.\n")
+
+    grid = enterSudoku(xSize,ySize)
+    if (grid == None):
+        return
+
+    mainGrid = grid
+
     prettyPrintGrid(mainGrid, xDiv, yDiv)
     
 if __name__ == "__main__":
