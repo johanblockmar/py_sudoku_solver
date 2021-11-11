99f90f8ac779e767e4a3f58a7545d2fe12a14d7a
Johan Blockmar <johan.blockmar@flexworks.com>
2021-11-11 20:57:48 +0100

    Added solver logic.

--- py_sudoku_solver.py
+++ py_sudoku_solver.py
@@ -1,4 +1,6 @@
 
+import time
+
 xSize = 9
 xDiv = 3
 ySize = 9
@@ -79,18 +81,80 @@ def enterSudoku(sizeX, sizeY, sudokuString = None):
 
     return grid
 
+def getLineNumbers(grid,x,y):
+    lineNumbers = []
+    for i in range(len(grid[y])):
+        if(grid[y][i] != 0):
+            lineNumbers.append(grid[y][i])
+
+    return lineNumbers
+
+def getColumnNumbers(grid,x,y):
+    columnNumbers = []
+    for j in range(len(grid)):
+        if(grid[j][x] != 0):
+            columnNumbers.append(grid[j][x])
+
+    return columnNumbers
+
+def getSquareNumbers(grid, x, y, divX, divY):
+    squareNumbers = []
+    
+    squareXmin = divX * int(float(x) / divX)
+    squareXmax = squareXmin + divX
+    squareYmin = divY * int(float(y) / divY)
+    squareYmax = squareYmin + divY
+    
+    for j in range(squareYmin,squareYmax):
+        for i in range(squareXmin,squareXmax):
+            if(grid[j][i] != 0):
+                squareNumbers.append(grid[j][i])
+
+    return squareNumbers
+
+    
+
 def main():
     print("Welcome to the Sudoku Solver.\n")
 
-    grid = enterSudoku(xSize,ySize,testString)
+    grid = enterSudoku(xSize,ySize)
     if (grid == None):
         return
 
     mainGrid = grid
 
-    prettyPrintGrid(mainGrid, xDiv, yDiv)
+    passes = 0
+    done = False
+
+    start = time.time()
+    while(not done):
+        done = True
+        passes += 1
+        prettyPrintGrid(mainGrid, xDiv, yDiv)
+        print("-----------------------")
+
+        for y in range(len(mainGrid)):
+            for x in range(len(mainGrid[0])):
+                if(mainGrid[y][x] == 0):
+                    done = False
+                    ln = getLineNumbers(mainGrid, x, y)
+                    cn = getColumnNumbers(mainGrid, x, y)
+                    sn = getSquareNumbers(grid, x, y, xDiv, yDiv)
+
+                    numbers = ln + cn + sn
+                    numbers = sorted(numbers)
+                    
+                    possible = []
+                    for i in range(1,10):
+                        if(i not in numbers):
+                            possible.append(i)
+                    
+                    if(len(possible) == 1):
+                        mainGrid[y][x] = possible[0]
+
+    print("Solved in {} passes.\nTime: {} ms".format(passes, (time.time()-start)*1000))
     
 if __name__ == "__main__":
     main()
 
-    input()
+    #input()
